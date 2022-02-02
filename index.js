17b4252676a7cd67c00714d13aba7876f3bf72ce
@georgemccadden <georgemccadden@gmail.com>
2022-02-01 22:10:21 -0800

    Just testing some syntax

---
+++ index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <ul>
+    <li>Land on planet React</li>
+    <li>Introduce ourselves to the Reactors (inhabitants of planet React)</li>
+    <li>Determine if they are hostile or not</li>
+    <li>Choose a temporary base camp</li>
+  </ul>,
+  document.getElementById("root")
+);
