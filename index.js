95311f9e643fb0a3865a03543874417281924982
@georgemccadden <georgemccadden@gmail.com>
2022-02-04 12:15:09 -0800

    Creating separate components for the web page

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+function Header() {
+  return (
+    <div>
+      <nav>
+        <img
+          src="react-logo.png"
+          alt="blue atom with four rings surrounding a blue nucleus"
+          width="60"
+          height="60"
+        />
+      </nav>
+    </div>
+  );
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons for relearning React :</h1>
+      <ol>
+        <li>I didn't have much time to learn it properly the first time</li>
+        <li>I'll be able to rebuild the components that I made previously</li>
+        <li>It will help me to get a position at a great company</li>
+        <li>
+          I will be one step closer to becoming as great a developer as those at
+          Meta
+        </li>
+      </ol>
+    </div>
+  );
+}
+
 function LandingPage() {
   return (
     <div>
-      <img
-        src="react-logo.png"
-        alt="blue atom with four rings surrounding a blue nucleus"
-        width="50"
-        height="50"
-      />
-      <h1>Reasons why I'm excited to learn React :</h1>
-      <ul>
-        <li>It was created and is maintained by Facebook</li>
-        <li>I will become a better front-end developer</li>
-        <li>Prospective jobs will become more available to me</li>
-        <li>I'll be able to build anything I can think of more easily</li>
-      </ul>
+      <Header />
+      <MainContent />
+      <footer>
+        <small>© 2022 Lathan development. All rights reserved.</small>
+      </footer>
     </div>
   );
 }
